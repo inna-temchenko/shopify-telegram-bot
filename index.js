971aea532fe575b64a7eaeedd8425af40c7da402
inna-temchenko <153179897+inna-temchenko@users.noreply.github.com>
2026-02-23 17:17:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.post('/webhook', async (req, res) => {
   res.sendStatus(200);
 });
 
+console.log(TELEGRAM_TOKEN,CHAT_ID)
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
