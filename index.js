35a5be8cc0e5fb771753d9f9e26883bece4df81f
inna-temchenko <153179897+inna-temchenko@users.noreply.github.com>
2026-02-23 21:57:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,25 +10,25 @@ app.post('/webhook', async (req, res) => {
   console.log(JSON.stringify(order));
   const items = order?.line_items?.map(({ name, current_quantity }) => `${name} ${current_quantity}шт.`).join(', ');
   const recipient = order?.shipping_address ? `
-Ім'я: ${order.shipping_address?.first_name} ${order.shipping_address?.last_name}
-Номер телефону: ${order.shipping_address?.phone || 'не вказано'}
-Адреса: ${order.shipping_address?.city}, ${order.shipping_address?.address1}
-Номер квартири: ${order.shipping_address?.address2 || 'не вказано'}
-  ` : `
-Ім'я: ${order.billing_address?.first_name} ${order.billing_address?.last_name}
-Номер телефону: ${order.billing_address?.phone || 'не вказано'}
+    Ім'я: ${order.shipping_address?.first_name} ${order.shipping_address?.last_name}
+    Номер телефону: ${order.shipping_address?.phone || 'не вказано'}
+    Адреса: ${order.shipping_address?.city}, ${order.shipping_address?.address1}
+    Номер квартири: ${order.shipping_address?.address2 || 'не вказано'}
+      ` : `
+    Ім'я: ${order.billing_address?.first_name} ${order.billing_address?.last_name}
+    Номер телефону: ${order.billing_address?.phone || 'не вказано'}
   `;
   const deliveryInfo = order.shipping_lines[0].code === 'Local Delivery' ? "Доставка кур'єром" : order.shipping_lines[0].code === 'Freesia' ? 'Самовивіз' : 'Не вказано';
  
   const message = `
-🌷 Нове замовлення!
-Замовлення: #${order.order_number}
-Номер телефону замовника: ${order.customer?.phone}
-  📦 Інформація про отримувача
-  ${recipient}
-Тип доставки: ${deliveryInfo}
-Товари: ${items}
-Сума: ${order.total_price} ${order.currency}
+    🌷 Нове замовлення!
+    Замовлення: #${order.order_number}
+    Номер телефону замовника: ${order.customer?.phone}
+      📦 Інформація про отримувача
+      ${recipient}
+    Тип доставки: ${deliveryInfo}
+    Товари: ${items}
+    Сума: ${order.total_price} ${order.currency}
   `;
 
   await fetch(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
