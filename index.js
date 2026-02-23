50f2c5805055e7a3bea84fc49d4db5965c8ab7ae
inna-temchenko <153179897+inna-temchenko@users.noreply.github.com>
2026-02-23 21:57:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,19 +8,15 @@ const CHAT_ID = process.env.CHAT_ID;
 app.post('/webhook', async (req, res) => {
   const order = req.body;
   console.log(JSON.stringify(order));
-const items = order?.line_items?.reduce((msg, value, index) => {
-  return msg + value.name + (index < order.line_items.length - 1 ? ', ' : '');
-}, '');
+  const items = order?.line_items?.map(({ name, current_quantity }) => `${name} ${current_quantity}шт.`).join(', ');
   const recipient = order?.shipping_address ? `
-  📦 Інформація про отримувача
-  Ім'я: ${order.shipping_address?.first_name} ${order.shipping_address?.last_name}
-  Номер телефону: ${order.shipping_address?.phone || 'не вказано'}
-  Адреса: ${order.shipping_address?.city}, ${order.shipping_address?.address1}
-  Номер квартири: ${order.shipping_address?.address2 || 'не вказано'}
+Ім'я: ${order.shipping_address?.first_name} ${order.shipping_address?.last_name}
+Номер телефону: ${order.shipping_address?.phone || 'не вказано'}
+Адреса: ${order.shipping_address?.city}, ${order.shipping_address?.address1}
+Номер квартири: ${order.shipping_address?.address2 || 'не вказано'}
   ` : `
-  📦 Інформація про отримувача
-  Ім'я: ${order.billing_address?.first_name} ${order.billing_address?.last_name}
-  Номер телефону: ${order.billing_address?.phone || 'не вказано'}
+Ім'я: ${order.billing_address?.first_name} ${order.billing_address?.last_name}
+Номер телефону: ${order.billing_address?.phone || 'не вказано'}
   `;
   const deliveryInfo = order.shipping_lines[0].code === 'Local Delivery' ? "Доставка кур'єром" : order.shipping_lines[0].code === 'Freesia' ? 'Самовивіз' : 'Не вказано';
  
@@ -28,7 +24,8 @@ const items = order?.line_items?.reduce((msg, value, index) => {
 🌷 Нове замовлення!
 Замовлення: #${order.order_number}
 Номер телефону замовника: ${order.customer?.phone}
-${recipient}
+  📦 Інформація про отримувача
+  ${recipient}
 Тип доставки: ${deliveryInfo}
 Товари: ${items}
 Сума: ${order.total_price} ${order.currency}
