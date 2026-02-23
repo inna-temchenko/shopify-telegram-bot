86ccf61569fd2bbc3a8f35ed5665c3bb11fb92b1
inna-temchenko <153179897+inna-temchenko@users.noreply.github.com>
2026-02-23 17:39:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const CHAT_ID = process.env.CHAT_ID;
 
 app.post('/webhook', async (req, res) => {
   const order = req.body;
-  
+  console.log(order)
   const message = `
-🛍 Нове замовлення!
-📦 Номер: #${order.order_number}
-👤 Клієнт: ${order.billing_address?.first_name} ${order.billing_address?.last_name}
-📱 Телефон: ${order.billing_address?.phone || 'не вказано'}
-💰 Сума: ${order.total_price} ${order.currency}
-📍 Місто: ${order.billing_address?.city}
+🌷 Нове замовлення!
+Номер замовлення: #${order.order_number}
+Ім'я отримувача: ${order.billing_address?.first_name} ${order.billing_address?.last_name}
+Телефон отримувача: ${order.billing_address?.phone || 'не вказано'}
+Сума: ${order.total_price} ${order.currency}
+Місто: ${order.billing_address?.city}
   `;
 
   await fetch(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
